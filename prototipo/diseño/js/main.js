998e668621a56e1516288b05f94e70fa0b042606
edwinpuco <edwin22@live.com>
2019-06-30 18:43:42 -0500

    corrección a estilos de menú móvil

--- prototipo/diseño/js/main.js
+++ prototipo/diseño/js/main.js
@@ -1,44 +1,23 @@
-(function() {
-  "use strict";
-  document.addEventListener('DOMContentLoaded', function(){
-    console.log("listo");
-    var nombreUno=document.getElementById('nombre1');
-    var nombreDos=document.getElementById('nombre2');
-    var apellidoUno=document.getElementById('apellido1');
-    var apellidoDos=document.getElementById('apellido2');
-    var fecha=document.getElementById('fechaNacimiento');
-    var celular=document.getElementById('telefono');
-    var email=document.getElementById('correo');
-    var pass=document.getElementById('password');
-    var repass=document.getElementById('confirmacion');
-    var errorDiv=document.getElementById('error');
-    nombreUno.addEventListener('blur',validarCampos);
-    nombreDos.addEventListener('blur',validarCampos);
-    apellidoUno.addEventListener('blur',validarCampos);
-    apellidoDos.addEventListener('blur',validarCampos);
-    email.addEventListener('blur',validarCampos);
-    email.addEventListener('blur',validarEmail);
-    function validarEmail(){
-      if(this.value.indexOf('@')>-1){
-        errorDiv.style.display='none';
-        this.style.border='1px solid #cccccc';
-      }else{
-        errorDiv.style.display='block';
-        errorDiv.innerHTML='ESTE CAMPO DEBE CONTENER @';
-        this.style.border='1px solid red';
-        errorDiv.style.border='1px solid red';
-      }
-    }
-    function validarCampos(){
-      if(this.value==''){
-        errorDiv.style.display='block';
-        errorDiv.innerHTML='ESTE CAMPO ES OBLIGATORIO';
-        this.style.border='1px solid red';
-        errorDiv.style.border='1px solid red';
-      }else{
-        errorDiv.style.display='none';
-        this.style.border='1px solid #cccccc';
-      }
+$(function() {
+  var windowHeight=$(window).height();
+  var barraAltura=$('.barra').innerHeight();
+  //console.log(windowHeight);
+  //console.log(barraAltura);
+  $(window).scroll(function(){
+    var scroll=$(window).scrollTop();
+    //console.log(scroll);
+    if(scroll<windowHeight){
+      $('.barra').addClass('fixed');
+      $('body').css({'margin-top':barraAltura+'px'});
+      //console.log('altura de pantalla rebasada')
+      //$('.barra').removeClass('fixed');
+      //$('body').css({'margin-top':'0px'});
+    }else{
+      //console.log('aun no')
+
     }
   });
-})();
+  $('.menu-movil').on('click',function(){
+    $('.navegacion-principal').slideToggle();
+  });
+});
